f592b4b70a9cc0516391deb67707c463d506ffd6
Soroosh Tanzadeh <soroosh081@gmail.com>
2020-11-08 13:14:56 +0330

    Click Outside to hide

--- src/tselect/Tselect.vue
+++ src/tselect/Tselect.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="tselect" :dir="direction">
-    <div class="tselect_title" ref="titleSection" @click="openDropdown">
+    <div class="tselect_title" ref="titleSection" v-click-outside="close" @click="openDropdown">
         <div class="title-text">
             <span v-if="selectedItem === undefined">
                 <slot>
@@ -42,6 +42,30 @@ export default {
         value(newValue) {
             this.selectedItem = newValue;
         },
+    },
+    directives: {
+        'click-outside': {
+            bind: function (el, binding) {
+                console.log(binding);
+                // Define ourClickEventHandler
+                const ourClickEventHandler = event => {
+                    if (!el.contains(event.target) && el !== event.target) {
+                        // as we are attaching an click event listern to the document (below)
+                        // ensure the events target is outside the element or a child of it
+                        binding.value(event); // before binding it
+                    }
+                };
+                // attached the handler to the element so we can remove it later easily
+                el.__vueClickEventHandler__ = ourClickEventHandler;
+
+                // attaching ourClickEventHandler to a listener on the document here
+                document.addEventListener("click", ourClickEventHandler);
+            },
+            unbind: function (el) {
+                // Remove Event Listener
+                document.removeEventListener("click", el.__vueClickEventHandler__);
+            }
+        }
     },
     props: {
         newItemPlaceholder: {
@@ -90,6 +114,10 @@ export default {
     },
     name: "Tselect",
     methods: {
+        close(event) {
+            this.showItems = false;
+            this.$refs.titleSection.classList.remove("active");
+        },
         isChecked(item) {
             if (this.selectedItem !== undefined) {
                 return (this.selectedItem[this.valueOption] === item[this.valueOption]);
@@ -117,5 +145,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "./tselect.scss";
 </style>
